9dc88b2f9c666616b1ddc78803c446a70617116a
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-10-18 10:11:53 -0400

    Added Login route

--- routes/auth.js
+++ routes/auth.js
@@ -22,6 +22,20 @@ router.post("/register", async (req, res) => {
     }
 })
 // async is used because we don't know how long it will take for the user to become registered within the database.
+
+
 //Login
+router.post("/login", async (req, res) => {
+    try{
+        const user = await User.findOne({username: req.body.username})
+        !user && res.status(400).json("Wrong Combination of Username and Password. Please Try Again.")
+        const validated = await bcrypt.compare(req.body.password, user.password)
+        !validated && res.status(400).json("Wrong Combination of Username and Password. Please Try Again.")
+        const {password, ...others} = user.toSource(); //._doc function added to avoid showing far more information than needed in our attempt to remove the password from the requested information. This method is deprecated.
+        res.status(200).json(others);
+    } catch(err){
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router
