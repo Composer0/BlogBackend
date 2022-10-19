985e44c8ab66005b6509b6102dcf905471c54a82
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-10-19 13:25:15 -0400

    New User routes
    Added Update, Delete, and Get routes in regards to user accounts. Tested each with Postman and Railway.app MongoDB. All work at this time.
    Updated index.js to include usersRoute.
    Left a comment that address deleting posts. Once Posts route is enabled this option will be enabled.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require('mongoose');
 const authRoute = require("./routes/auth");
+const usersRoute = require("./routes/users");
 
 dotenv.config();
 app.use(express.json()); //allows json objects to be sent.
@@ -10,6 +11,7 @@ app.use(express.json()); //allows json objects to be sent.
 mongoose.connect(process.env.MONGO_URL).then(console.log("Connected to Railway.app")).catch((err) => console.log(err));
 // Mongoose always treats creating a new index as true so it is no longer required in the code. If false then you will need to indicate it in the code. Otherwise you are absolutely fine.
 app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
 
 console.log("Orion")
 
@@ -22,6 +24,6 @@ app.use("/Orion", (req, res) => {
 })
 
 
-app.listen("5001", () => {
+app.listen("4274", () => {
     console.log("Backend is running");
 })
