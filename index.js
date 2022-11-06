b688b0a2cb2fbf38241cc861e655bf9b02a9dc1e
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-11-06 16:33:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,49 +13,49 @@ const cors = require('cors')
 
 
 dotenv.config();
-app.use(express.json()); //allows json objects to be sent.
 app.use("/images", express.static(path.join(__dirname, "/images")))
 // app.use(express.static(path.resolve(__dirname, "../client/build"))); //Node will serve files from the React app.
 // app.get("/api", (req,res) => {
-//     res.json({message: "I think we're in business"}); 
-// }) //Handle GET requests to /aApi route.
-// app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-// }) // All other Get requests not handled before will return our React app.
+    //     res.json({message: "I think we're in business"}); 
+    // }) //Handle GET requests to /aApi route.
+    // app.get('*', (req, res) => {
+        //     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+        // }) // All other Get requests not handled before will return our React app.
+        
+        app.use(cors());
+        
+        const MONGO_URL = `mongodb://${process.env.MONGOUSER }:${ process.env.MONGOPASSWORD }@${ process.env.MONGOHOST }:${ process.env.MONGOPORT }`
+        mongoose.connect(MONGO_URL).then(console.log("Connected to Railway.app")).catch((err) => console.log(err));
+        // Mongoose always treats creating a new index as true so it is no longer required in the code. If false then you will need to indicate it in the code. Otherwise you are absolutely fine.
+        
+        //Multer code being used for picture upload and storage on the server.
+        const storage = multer.diskStorage({
+            destination:(req, file, cb) => {
+                cb(null,"images")
+            }, filename:(req, file,cb) =>{
+                cb(null, req.body.name)
+            },
+        }); //cb stands for callback function
 
-app.use(cors());
-
-const MONGO_URL = `mongodb://${process.env.MONGOUSER }:${ process.env.MONGOPASSWORD }@${ process.env.MONGOHOST }:${ process.env.MONGOPORT }`
-mongoose.connect(MONGO_URL).then(console.log("Connected to Railway.app")).catch((err) => console.log(err));
-// Mongoose always treats creating a new index as true so it is no longer required in the code. If false then you will need to indicate it in the code. Otherwise you are absolutely fine.
-
-//Multer code being used for picture upload and storage on the server.
-const storage = multer.diskStorage({
-    destination:(req, file, cb) => {
-        cb(null,"images")
-    }, filename:(req, file,cb) =>{
-        cb(null, req.body.name)
-    },
-}); //cb stands for callback function
-
-const upload = multer({storage:storage});
-app.post("/api/upload", upload.single("file"), (req, res)=>{
-    res.status(200).json("file has been uploaded.")
+        const upload = multer({storage:storage});
+        app.post("/api/upload", upload.single("file"), (req, res)=>{
+            res.status(200).json("file has been uploaded.")
 });
 
 app.use("/images", express.static(path.join(__dirname,"./images")))
 //End of Multer code.
 
 
+app.use(express.json()); //allows json objects to be sent.
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/posts", postsRoute);
 app.use("/api/categories", categoriesRoute);
 
-app.get('/', (req, res) => {
-    res.console.log("Hello Creators' Blog")
-})
+// app.use('/', (req, res) => {
+//     res.console.log("Hello Creators' Blog")
+// })
 
 // app.get('/', (req, res) => {
 //     res.json({msg: 'Hello world!!!'});
